9871fb910d0dce46804a7d54fe9c6679cd6bc2cc
Srajan  Sharma <81655385+srajan163@users.noreply.github.com>
2025-03-20 16:48:54 +0000

    datatypes

---
+++ datatype.js
@@ -0,0 +1,18 @@
+"use-strict" // this is used to enable strict mode
+//treat code as newer version of javascript
+
+//alert( 3+3) this is not used in nodejs.
+//code readability should be good'
+let name="srajan" //string
+let age=21 //number
+let isApproved=true //boolean
+let firstName=undefined
+let lastName=null
+
+//null it is standaolone value
+//undefined is a type and value
+//null is used to clear the value of variable
+//undefined is used to declare a variable without value
+console.log(typeof "srajan")
+//null is an object
+//undefined is undefined
---
+++ variables.js
@@ -0,0 +1,15 @@
+const accountId=14453;
+let accountEmail="rishi@google";
+var accountPassword ="123456";
+console.log(accountId,accountEmail,accountPassword);
+console.table({accountId,accountEmail,accountPassword});
+
+/*
+
+prefer not to user var, use let instead
+let is block scoped
+var is function scoped
+const is block scoped and cannot be reassigned
+const is used for values that are not going to changed
+
+*/
