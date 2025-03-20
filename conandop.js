1ba9d59871c0cc0e6a87ad13c9b43e5f4cc9d68f
Srajan  Sharma <81655385+srajan163@users.noreply.github.com>
2025-03-20 17:00:53 +0000

    conversion of data types

---
+++ conandop.js
@@ -0,0 +1,11 @@
+//conversions and operations
+//Nan not a number
+
+let score="100"
+let valueinNumber=Number(score)
+console.log(valueinNumber)
+console.log(typeof valueinNumber)
+// score="100aa" return Nan
+//true 1 false 0
+//"" => false in boolean
+//"hsdhdh" => true in boolean
