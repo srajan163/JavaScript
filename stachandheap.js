b19653d2597d2e2ef366d0ec2532457c12571cf3
Srajan  Sharma <81655385+srajan163@users.noreply.github.com>
2025-03-24 16:59:00 +0000

    stringd

---
+++ stachandheap.js
@@ -0,0 +1,4 @@
+//Stack and Heap
+//Stack is a linear data structure which follows a particular order in which the operations are performed. The order may be LIFO(Last In First Out) or FILO(First In Last Out).
+//Heap is a memory used by programming languages to store global variables. It is a large pool of memory used to store objects, and it manages memory allocation and deallocation for you.
+//Stack(Primative) , Heap(Non-Primative) 
